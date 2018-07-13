1b0b6a9db03ff853fa38e6c823ddb2e57f442f38
BHONESH CHAWLA <rajachawla778@gmail.com>
2018-07-13 19:18:31 +0530

    Update and rename AMORTIZED1 to AMORTIZED1.cpp

--- AMORTIZED1.cpp
+++ AMORTIZED1.cpp
@@ -5,7 +5,7 @@
             }
         }
         
-answer :- 
+/*answer :- 
 The first loop will run N times . 
 For the second loop we can see there is arr[0] < arr[0] which is always false.
-so total time complexity will be O(N).
+so total time complexity will be O(N).*/
